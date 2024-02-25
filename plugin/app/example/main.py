bd8dcb8ae548e8f8e6503026a78c6dd2cd7d21bc
q1877242571 <62864593+q1877242571@users.noreply.github.com>
2024-02-26 03:33:31 +0800

    Add files via upload

---
+++ plugin/app/example/main.py
@@ -0,0 +1,10 @@
+async def group_message(message_data, event):
+    #plugin to do
+    pass
+    #await event.send_group_msg(group_id=message_data['group_id'], data="测试成功") #调用bot.api中的函数发送消息（需要使用await异步）
+
+
+async def private_message(message_data, event):
+    #plugin to do
+    pass
+    #await event.send_private_msg(user_id=message_data['user_id'], data="测试成功") #调用bot.api中的函数发送消息（需要使用await异步）
