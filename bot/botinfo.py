c4b935af18dde91a94d70e30f26309bc7b923f45
lacimoc <62864593+lacimoc@users.noreply.github.com>
2024-03-19 19:45:32 +0800

    update: 机器人版本信息

--- bot/botinfo.py
+++ bot/botinfo.py
@@ -1,8 +1,8 @@
 async def main(message_data, event, _log, login_info):
     if message_data.msg_type == 'group':
         if message_data.msg == ".bot":
-            await event.send_group_msg(group_id=message_data.group_id, data=f"Bot {login_info['data']['nickname']} Work on LLOneBot Designed by HOMO-Bot Ver.0.3.0(11) [Python 3.11.8 For HOMO-Bot]")
+            await event.send_group_msg(group_id=message_data.group_id, data=f"Bot {login_info['data']['nickname']} Work on LLOneBot Designed by HOMO-Bot Ver.0.3.1(12) [Python 3.11.8 For HOMO-Bot]")
 
     if message_data.msg_type == 'private':
         if message_data.msg == ".bot":
-            await event.send_private_msg(user_id=message_data.user_id, data=f"Bot {login_info['data']['nickname']} Work on LLOneBot Designed by HOMO-Bot Ver.0.3.0(11) [Python 3.11.8 For HOMO-Bot]")
+            await event.send_private_msg(user_id=message_data.user_id, data=f"Bot {login_info['data']['nickname']} Work on LLOneBot Designed by HOMO-Bot Ver.0.3.1(12) [Python 3.11.8 For HOMO-Bot]")
